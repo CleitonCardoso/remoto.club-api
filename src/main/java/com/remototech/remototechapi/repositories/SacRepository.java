32abcafe73aaebdfb16c6085006f96bc4fe4bc8f
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-10-02 22:42:52 -0300

    Alterando tipo passado para o repository de Long para UUID

--- src/main/java/com/remototech/remototechapi/repositories/SacRepository.java
+++ src/main/java/com/remototech/remototechapi/repositories/SacRepository.java
@@ -1,11 +1,14 @@
 package com.remototech.remototechapi.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.remototech.remototechapi.entities.Login;
 import com.remototech.remototechapi.entities.Sac;
 
 @Repository
-public interface SacRepository extends JpaRepository<Sac, Long>{
-
+public interface SacRepository extends JpaRepository<Sac, UUID>{
+	
 }
